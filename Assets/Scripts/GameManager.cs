537b1a173810d186a85f8ea0fcdcb564184e7fce
Sam McKinley <sammckin@uat.edu>
2020-03-23 12:18:44 -0700

    Added more Files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int currentScene = 0;
+
+    void Awake()
+    {
+
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Warning: A second gamemanager was detected and destroyed");
+            }
+
+        } 
+    }
+    public void LoadLevel(string levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+        //Potentially dangerous
+        currentScene++;
+    }
+
+
+    public void LoadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
+ 
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,17 +7,20 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D rb2d;
     private Transform tf;
     private SpriteRenderer sr;
+    private Animator animator;
     
     public float speed = 5.0f;
     public float jumpForce = 10.0f;
     public Transform GroundPoint;
     public bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
         tf = gameObject.GetComponent<Transform>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,7 +28,20 @@ public class PlayerControl : MonoBehaviour
     {
         float xMovement = Input.GetAxis("Horizontal") * speed;
         rb2d.velocity = new Vector2(xMovement, rb2d.velocity.y);
-        sr.flipX = rb2d.velocity.x < 0;
+        if (xMovement > 0)
+        {
+            sr.flipX = false;
+            animator.Play("PlayerWalk");
+        }
+        else if (xMovement < 0)
+        {
+            sr.flipX = true;
+            animator.Play("PlayerWalk");
+        }
+        else
+        {
+            animator.Play("PlayerIdle");
+        }
         //Detect if player is on the ground
         bool isGrounded = false;
         RaycastHit2D hitInfo = Physics2D.Raycast(GroundPoint.position, Vector2.down, 0.1f);
